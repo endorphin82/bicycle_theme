46c9aa852cbc7a1598a3b14de2a899377b2e35fe
endorphin82 <endorphin82@gmail.com>
2020-01-09 22:21:26 +0200

    tinypng

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function () {
- console.log('jQuery');
+  $('.icon-menu,.menu__body').click(function (event) {
+    $('.icon-menu,.menu__body').toggleClass('active');
+    $('body').toggleClass('lock');
+  });
+
+  (function ibg() {
+    $.each($('.ibg'), function (index, val) {
+      if ($(this).find('img').length > 0) {
+        $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+      }
+    });
+  })();
 });
